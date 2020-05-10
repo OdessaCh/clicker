1f5804588695b31ebe249b28130097203ba8e83b
Odessa Chesneau <odessa.chesneau@gmail.com>
2020-05-10 15:30:37 +0200

    database linked to website

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @mes_utilisateurs = User.all
   end
+
+  def create
+    User.create name: params[:name]
+    redirect_to "/users"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'users' => 'users#index'
+  post 'users' => 'users#create'
   root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
- 
-# Rails.application.routes.draw do
-#   get 'users/index'
-#   get 'admin' => 'pages#admin'
-# end

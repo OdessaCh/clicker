9eaba73c5fb1c41dc787856427ad7a193e8c0c91
Odessa Chesneau <odessa.chesneau@gmail.com>
2020-05-26 13:28:27 +0200

    ennemi score increments

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,4 +1,14 @@
-window.scoreUp = function() {
-  let score = this.document.getElementById('points');  
+let check = false;
+window.scoreUp = function () {
+  let score = this.document.getElementById('points');
   score.innerText++;
+  if (!check) {
+    scoreEnnemi();
+  }
+
+}
+window.scoreEnnemi = function () {
+  check = true;
+  let score = this.document.getElementById('pointsEnnemi');
+  this.setInterval(function () { score.innerText++; }, 500);
 }

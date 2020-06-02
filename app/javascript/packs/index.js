aaa2eb0a65809023b29540b80c54725eeef65dc0
Odessa Chesneau <odessa.chesneau@gmail.com>
2020-06-02 12:20:17 +0200

    highiest score

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,13 +4,20 @@ let scoreAdverse;
 let compteur = 0;
 let pointParSeconde = 1;
 let pointsEnnemiParSeconde = 1;
+let meilleurScore;
+let faibleScore;
 
 window.scoreUp = function () {
     scoreAdverse = this.document.getElementById('pointsEnnemi');
+    scoreJoueur = this.document.getElementById('points');
     augmentationScoreJoueur(1);
     compteur++;
+
     if (compteur == 15) {
         augmentationScoreJoueur(1025);
+    }
+    if (this.parseInt(scoreJoueur.innerText, 10) > this.parseInt(scoreAdverse.innerText, 10)) {
+        meilleurJoueur(scoreJoueur, scoreAdverse);
     }
     if (compteur === 50) {
         pointParSeconde = 53;
@@ -31,6 +38,10 @@ window.lancerScore = function () {
         if (scoreAdverse.innerText == 10) {
             augmenterScoreEnnemi(806);
         }
+
+        if (parseInt(scoreAdverse.innerText, 10) > parseInt(scoreJoueur.innerText, 10)) {
+            meilleurJoueur(scoreAdverse, scoreJoueur);
+        }
         if (scoreAdverse.innerText >= 820) {
             augmenterScoreEnnemi(84);
         }
@@ -39,16 +50,21 @@ window.lancerScore = function () {
 }
 
 window.augmenterScoreEnnemi = function (augmentation) {
-    scoreAdverse = this.document.getElementById('pointsEnnemi');
     let stockInt = this.parseInt(scoreAdverse.innerText, 10);
     stockInt += augmentation;
     scoreAdverse.innerText = stockInt;
-
 }
 
 window.augmentationScoreJoueur = function (augmentation) {
-    scoreJoueur = this.document.getElementById('points');
     let stockInt = this.parseInt(scoreJoueur.innerText, 10);
     stockInt += augmentation;
     scoreJoueur.innerText = stockInt;
 }
+
+window.meilleurJoueur = function (plusHautScore, plusBasScore) {
+    meilleurScore = plusHautScore;
+    meilleurScore.style.fontSize = '1.4em';
+    faibleScore = plusBasScore;
+    plusBasScore.style.fontSize = '1em';
+}
+
